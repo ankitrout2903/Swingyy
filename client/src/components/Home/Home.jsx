2ef715275efacc18064e83105764c53f7a9a8d0d
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-07-01 01:12:24 +0530

    summm

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,8 @@ import Hello from './hello.gif';
 
 export default function Home(){
     return (
-        <><div className="home">Welcome to Swinngy Chat app!!</div>
-        <img src={Hello} alt="" /></>
+        <><img src={Hello} alt="" />
+        <div className="home">Welcome to Swingy Chat app!!</div>
+        </>
     )
 }
