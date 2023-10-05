c5f81f78aed9cb94f0cf0edc33e8e9c48651506e
Himanshu Choudhary <22himanshu14@gmail.com>
2023-10-05 22:43:17 +0530

    [Enhancement] Storing hashed passwords in database instead of plain text [Fixes] - 'email' variable replaced with 'mail', causing error in API - generated uuid, which was required for registering user

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,29 +1,41 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const connection = require('../db')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const connection = require("../db");
+const uuid = require("uuid");
 
 // Controller method to register a new user
 module.exports.register = async (req, res, next) => {
   try {
-    const { username, mail, password, uid } = req.body;
+    const { username, email, password } = req.body;
 
-   /**
-    * I changed the DB a little bit, I have removed the NOT NULL requirement in password because if you're signing in with google, you won't have their password.
-    * the password field can be checked for not null if you create a separate login page though.
-    */
+    /**
+     * I changed the DB a little bit, I have removed the NOT NULL requirement in password because if you're signing in with google, you won't have their password.
+     * the password field can be checked for not null if you create a separate login page though.
+     */
 
-      // Create a new user in the database
-      const insertQuery = 'INSERT INTO user_table (email, username, password, uuid) VALUES (?, ?, ?, ?)';
-      const values = [mail, username, password, uid];
+    // Create a new user in the database
+    const uid = uuid.v4();
 
-      connection.query(insertQuery, values, (err, result) => {
-        if (err) {
-          console.error('Error adding user: ', err);
-          return res.json({ msg: 'Failed to register user', status: false });
-        }
-       //if registration is successful, return the user object
-        return res.json({ status: true, user: { username , mail, password, _id: result.insertId },message : "User registered successfully" });
+    // Hash the password
+    const salt = await bcrypt.genSalt(5);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // return res.json({ salt: salt, hashedPassword: hashedPassword });
 
+    const insertQuery =
+      "INSERT INTO user_table (email, username, password, uuid) VALUES (?, ?, ?, ?)";
+    const values = [email, username, hashedPassword, uid];
+
+    connection.query(insertQuery, values, (err, result) => {
+      if (err) {
+        console.error("Error adding user: ", err);
+        return res.json({ msg: "Failed to register user", status: false });
+      }
+      //if registration is successful, return the user object
+      return res.json({
+        status: true,
+        user: { username, mail: email, password, _id: result.insertId },
+        message: "User registered successfully",
+      });
     });
   } catch (ex) {
     next(ex); // Pass the error to the next error-handling middleware
@@ -31,10 +43,10 @@ module.exports.register = async (req, res, next) => {
 };
 
 /**
- * 
+ *
  * @param {mail} req mail of the user
  * @param {boolean} res status: boolean, user: {username, email, _id}
- * @param {*} next 
+ * @param {*} next
  */
 module.exports.checkUser = async (req, res, next) => {
   try {
@@ -42,24 +54,28 @@ module.exports.checkUser = async (req, res, next) => {
     const values = [mail];
 
     // Find the user by email
-    const selectQuery = 'SELECT * FROM user_table WHERE email = ?';
+    const selectQuery = "SELECT * FROM user_table WHERE email = ?";
     connection.query(selectQuery, values, (err, rows) => {
       if (err) {
-        console.error('Error retrieving user: ', err);
-        return res.json({ msg: 'An error occurred', status: false });
+        console.error("Error retrieving user: ", err);
+        return res.json({ msg: "An error occurred", status: false });
       }
       const user = rows[0];
 
       if (user) {
-        return res.json({ status: true, user: { username: user.username, email: user.email, _id: user.id }, message: 'User exists' });
+        return res.json({
+          status: true,
+          user: { username: user.username, email: user.email, _id: user.id },
+          message: "User exists",
+        });
       } else {
-        return res.json({ status: false, message: 'User does not exist' });
+        return res.json({ status: false, message: "User does not exist" });
       }
     });
   } catch (ex) {
     next(ex);
   }
-}
+};
 
 // Controller method to authenticate user login
 module.exports.login = async (req, res, next) => {
@@ -67,15 +83,15 @@ module.exports.login = async (req, res, next) => {
     const { username, password } = req.body;
 
     // Find the user by username
-    const selectQuery = 'SELECT * FROM user_table WHERE username = ?';
+    const selectQuery = "SELECT * FROM user_table WHERE username = ?";
     connection.query(selectQuery, [username], (err, rows) => {
       if (err) {
-        console.error('Error retrieving user: ', err);
-        return res.json({ msg: 'An error occurred', status: false });
+        console.error("Error retrieving user: ", err);
+        return res.json({ msg: "An error occurred", status: false });
       }
 
       if (rows.length === 0) {
-        return res.json({ msg: 'Invalid username/password', status: false });
+        return res.json({ msg: "Invalid username/password", status: false });
       }
 
       const user = rows[0];
@@ -83,21 +99,25 @@ module.exports.login = async (req, res, next) => {
       // Compare the provided password with the stored hashed password
       bcrypt.compare(password, user.password, (err, isValidPassword) => {
         if (err) {
-          console.error('Error comparing passwords: ', err);
-          return res.json({ msg: 'An error occurred', status: false });
+          console.error("Error comparing passwords: ", err);
+          return res.json({ msg: "An error occurred", status: false });
         }
 
         if (!isValidPassword) {
-          return res.json({ msg: 'Invalid username/password', status: false });
+          return res.json({ msg: "Invalid username/password", status: false });
         }
 
         // Generate JWT token
-        const token = jwt.sign({ username, _id: user.id }, process.env.TOKEN_KEY, {
-          expiresIn: 300,
-        });
+        const token = jwt.sign(
+          { username, _id: user.id },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: 300,
+          }
+        );
 
         // Set the token as a cookie
-        res.cookie('token', token, {
+        res.cookie("token", token, {
           httpOnly: true,
         });
 
@@ -122,21 +142,19 @@ module.exports.getAllUsers = async (req, res, next) => {
 
     // Retrieve all users except the current user
     // const selectQuery = 'SELECT email, username , photo_url FROM swingyy.user_table WHERE email != ?';
-    
-    const selectQuery = 'SELECT * FROM swingyy.user_table;';
+
+    const selectQuery = "SELECT * FROM swingyy.user_table;";
     connection.query(selectQuery, [email], (err, rows) => {
       if (err) {
-        console.error('Error retrieving users: ', err);
+        console.error("Error retrieving users: ", err);
         return res.json([]);
       }
       console.log(rows);
 
       // Sort the users by username in ascending order
       rows.sort((a, b) => a.username.localeCompare(b.username));
-      
 
       return res.json(rows);
-      
     });
   } catch (ex) {
     next(ex); // Pass the error to the next error-handling middleware
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,15 @@
-import { useEffect } from 'react';
-import { json, useNavigate } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { auth } from '../../firebase/firebaseConfig';
-import axios from 'axios';
-import googleIcon from './google-icon.png.png'; // Import the Google icon image
-import './style.css';
-import { checkUserRoute, loginRoute, registerRoute } from "../../utils/APIRoutes";
-import { FcGoogle } from 'react-icons/fc'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+import axios from "axios";
+import "./style.css";
+import { checkUserRoute, registerRoute } from "../../utils/APIRoutes";
+import { FcGoogle } from "react-icons/fc";
 
-export default function Login () {
+export default function Login() {
   const navigate = useNavigate();
   const provider = new GoogleAuthProvider();
   var username = null;
@@ -18,7 +17,7 @@ export default function Login () {
   var uid = null;
   var pass = null;
   var mail = null;
-  var existing = null
+  var existing = null;
   const toastProps = {
     className: "custom-toast",
     draggable: false,
@@ -27,37 +26,55 @@ export default function Login () {
     theme: "colored",
   };
 
-  async function validate ({ username, uid, password, mail }) {
-    existing = await axios.post(checkUserRoute, { //check if the user exists
-      mail,
-    }, {
-      withCredentials: true
-    });
-    if (existing.data.status === false) {
-      const data = await axios.post(registerRoute, { //if not create one.
-        username,
+  async function validate({ username, uid, password, mail }) {
+    existing = await axios.post(
+      checkUserRoute,
+      {
+        //check if the user exists
         mail,
-        password,
-        uid,
-      }, {
-        withCredentials: true
-      });
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    if (existing.data.status === false) {
+      const data = await axios.post(
+        registerRoute,
+        {
+          //if not create one.
+          username,
+          mail,
+          password,
+          uid,
+        },
+        {
+          withCredentials: true,
+        }
+      );
       localStorage.setItem("id", JSON.stringify(data.data.user._id));
     } else {
-      console.log('existing');
+      console.log("existing");
       const existUser = existing.data.user;
       const _id = existUser._id;
       console.log(_id);
-      console.log(existing.data)
+      console.log(existing.data);
       localStorage.setItem("_id", JSON.stringify(_id)); //if yes, add data to localstorage
       // localStorage.setItem("react-chat-user", JSON.stringify(existing.returnedUser));
     }
   }
 
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+
+  const navigateToRegister = () => {
+    navigate("/register");
+  };
+
   const signInWithGoogle = async () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log('in results:' + result);
+        console.log("in results:" + result);
         console.log(result.user);
         user = result.user;
         uid = user.uid;
@@ -69,13 +86,13 @@ export default function Login () {
           .then((token) => {
             // the idea is to add the user to our DB if they login with google, because otherwise we can't really do anything.
             validate({ username, uid, pass, mail });
-            console.log('Tokens: ' + token);
+            console.log("Tokens: " + token);
           })
           .catch((error) => {
             console.log(error);
           });
 
-        localStorage.setItem('react-chat-user', JSON.stringify(result.user));
+        localStorage.setItem("react-chat-user", JSON.stringify(result.user));
         console.log("User registered");
         navigate("/");
       })
@@ -85,18 +102,18 @@ export default function Login () {
   };
 
   useEffect(() => {
-    if (localStorage.getItem('react-chat-user')) {
+    if (localStorage.getItem("react-chat-user")) {
       navigate("/");
     }
   }, []);
 
   return (
-    <div class="wrapper">
-      <span class="borderLine"></span>
+    <div className="wrapper">
+      <span className="borderLine"></span>
       <h1>
         <span>S</span>wingyy
       </h1>
-      <div class="description">
+      <div className="description">
         <p>
           Swingyy is a real-time chat application that allows users to connect
           and communicate with their friends instantly. With Swingyy, you can
@@ -105,27 +122,33 @@ export default function Login () {
           friends.
         </p>
       </div>
-      <div className='signIn-container'>
+      <div className="signIn-container">
         <div>
-          <button type="submit" class="btn" onClick={ () => signInWithGoogle() }>
+          <button
+            className="btn"
+            onClick={() => {
+              navigate("/login");
+              console.log("clicked");
+            }}
+          >
             <div>
               <FcGoogle />
             </div>
           </button>
 
-          <div class="text">
+          <div className="text">
             <p>Login Now</p>
           </div>
         </div>
 
         <div>
-          <button type="submit" class="btn" >
+          <button className="btn" onClick={navigateToRegister}>
             <div>
               <FcGoogle />
             </div>
           </button>
 
-          <div class="text">
+          <div className="text">
             <p>Sign up</p>
           </div>
         </div>
